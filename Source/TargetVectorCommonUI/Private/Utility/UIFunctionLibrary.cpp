56b00a3acd1f3256d20e8a371ae8ba91906d6eb7
colorindarkness <urielballinas@gmail.com>
2022-10-12 03:26:08 -0700

    Created UIBlueprintFunctionLibrary (C++) and BFL_SaveGame. Created FieldDescriptionPanel to work with Fields in MainMenu Settings. Fixes to warnings. Made additional styles.

---
+++ Source/TargetVectorCommonUI/Private/Utility/UIFunctionLibrary.cpp
@@ -0,0 +1,15 @@
+// MIT
+
+
+#include "Utility/UIFunctionLibrary.h"
+#include "Engine/UserInterfaceSettings.h"
+
+void UUIFunctionLibrary::SetUIScale(float CustomUIScale)
+{
+	UUserInterfaceSettings* UISettings = GetMutableDefault<UUserInterfaceSettings>(UUserInterfaceSettings::StaticClass());
+
+	if (UISettings)
+	{
+		UISettings->ApplicationScale = CustomUIScale;
+	}
+}
---
+++ Source/TargetVectorCommonUI/Public/Utility/UIFunctionLibrary.h
@@ -0,0 +1,22 @@
+// MIT
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UIFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TARGETVECTORCOMMONUI_API UUIFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	// Custom UI scale 1.0f == 100%, 2.0f == 200%, 0.5f == 50% etc.
+	UFUNCTION(BlueprintCallable, Category = "User Interface")
+	static void SetUIScale(float CustomUIScale);
+	
+};
