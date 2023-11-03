09cc17522e07a87d9664bc42f976abd9daee6b90
colorindarkness <urielballinas@gmail.com>
2023-11-03 03:34:07 -0700

    Add Enum for Handedness Preference

--- Source/TargetVectorCommonUI/Public/Settings/Settings.h
+++ Source/TargetVectorCommonUI/Public/Settings/Settings.h
@@ -1,3 +1,71 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9910706d825d291042cc864024abf210257891f11fcc359c3596ec1d4aeca245
-size 2001
+#pragma once
+
+#include "Settings.generated.h"
+
+UENUM(BlueprintType)
+enum class EHandedness : uint8
+{
+	Left UMETA(DisplayName = "Left"),
+	Right UMETA(DisplayName = "Right"),
+	Count UMETA(Hidden)
+};
+ENUM_RANGE_BY_COUNT(EHandedness, EHandedness::Count);
+
+UENUM(BlueprintType)
+enum class EProfanityFilterLevel : uint8
+{
+	Disabled UMETA(DisplayName = "All Profanity"),
+	Basic UMETA(DisplayName = "Basic"),
+	Medium UMETA(DisplayName = "Medium"),
+	Large UMETA(DisplayName = "Strict"),
+	Count UMETA(Hidden)
+};
+ENUM_RANGE_BY_COUNT(EProfanityFilterLevel, EProfanityFilterLevel::Count);
+
+UENUM(BlueprintType)
+enum class EGoreFilterLevel : uint8
+{
+	Disabled UMETA(DisplayName = "All Gore"),
+	NoGuts UMETA(DisplayName = "No Guts"),
+	NoDismemberment UMETA(DisplayName = "No Dismemberment"),
+	NoLongDeaths UMETA(DisplayName = "No Long Deaths"),
+	NoGutsNoLongDeaths UMETA(DisplayName = "No Guts, No Long Deaths"),
+	NoGutsNoDismemberment UMETA(DisplayName = "No Guts, No Dismemberment"),
+	NoDismembermentNoLongDeaths UMETA(DisplayName = "No Dismemberment, No Long Deaths"),
+	None UMETA(DisplayName = "None"),
+	Count UMETA(Hidden)
+};
+ENUM_RANGE_BY_COUNT(EGoreFilterLevel, EGoreFilterLevel::Count);
+
+UENUM(BlueprintType)
+enum class EBloodFilterLevel : uint8
+{
+	Disabled UMETA(DisplayName = "All Blood"),
+	ImpactsOnly UMETA(DisplayName = "Impacts Only"),
+	RecoloredBlood UMETA(DisplayName = "Recolored Blood"),
+	RecoloredBloodImpactsOnly UMETA(DisplayName = "Recolored Blood, Impacts Only"),
+	None UMETA(DisplayName = "No Blood"),
+	Count UMETA(Hidden)
+};
+ENUM_RANGE_BY_COUNT(EBloodFilterLevel, EBloodFilterLevel::Count);
+
+UENUM(BlueprintType)
+enum class ENudityFilterLevel : uint8
+{
+	Disabled UMETA(DisplayName = "All Nudity"),
+	Basic UMETA(DisplayName = "Blurred"),
+	None UMETA(DisplayName = "None"),
+	Count UMETA(Hidden)
+};
+ENUM_RANGE_BY_COUNT(ENudityFilterLevel, ENudityFilterLevel::Count);
+
+UENUM(BlueprintType)
+enum class EColorBlindFilter : uint8
+{
+	Disabled UMETA(DisplayName = "Disabled"),
+	Protanope UMETA(DisplayName = "Protanope"),
+	Deuteranope UMETA(DisplayName = "Deuteranope"),
+	Tritanope UMETA(DisplayName = "Tritanope"),
+	Count UMETA(Hidden)
+};
+ENUM_RANGE_BY_COUNT(EColorBlindFilter, EColorBlindFilter::Count);
